573ee86f4d9512fc0184400cc000f5339068c29b
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-30 22:28:37 +0300

    end of day

--- src/com/kumsal/springdemo/AnnotationDemoApp.java
+++ src/com/kumsal/springdemo/AnnotationDemoApp.java
@@ -1,5 +1,13 @@
 package com.kumsal.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AnnotationDemoApp {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		Coach theCoach=context.getBean("thatSillyCoach",Coach.class);
+		System.out.println(theCoach.getDailyWorkout());
+		context.close();
+	}
 
 }
