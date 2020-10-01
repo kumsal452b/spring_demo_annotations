57d9db088893eeb801b7f34595e7b48b7b13e488
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-01 22:19:48 +0300

    Nothing

--- src/com/kumsal/springdemo/AnnotationDemoApp.java
+++ src/com/kumsal/springdemo/AnnotationDemoApp.java
@@ -7,6 +7,8 @@ public class AnnotationDemoApp {
 		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
 		Coach theCoach=context.getBean("tennisCoach",Coach.class);
 		System.out.println(theCoach.getDailyWorkout());
+		
+		System.out.println(theCoach.getDailyFortune());
 		context.close();
 	}
 
--- src/com/kumsal/springdemo/TennisCoach.java
+++ src/com/kumsal/springdemo/TennisCoach.java
@@ -1,14 +1,17 @@
 package com.kumsal.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
+	@Autowired
 	public TennisCoach(FortuneService theFotuneservice) {
 		this.fortuneService=theFotuneservice;
 	}
+	
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
