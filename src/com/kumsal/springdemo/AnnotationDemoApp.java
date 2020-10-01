3c4ad163709fe63b0ddeb23d8de9dd1cb5c73ece
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-01 21:35:35 +0300

    add new lines

--- src/com/kumsal/springdemo/AnnotationDemoApp.java
+++ src/com/kumsal/springdemo/AnnotationDemoApp.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class AnnotationDemoApp {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
-		Coach theCoach=context.getBean("thatSillyCoach",Coach.class);
+		Coach theCoach=context.getBean("tennisCoach",Coach.class);
 		System.out.println(theCoach.getDailyWorkout());
 		context.close();
 	}
