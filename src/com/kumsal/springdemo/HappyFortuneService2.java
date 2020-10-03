0aacceb10799c8ede6b318c65e30871392fc3f2a
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-03 10:27:07 +0300

    add new class

---
+++ src/com/kumsal/springdemo/HappyFortuneService2.java
@@ -0,0 +1,13 @@
+package com.kumsal.springdemo;
+
+public class HappyFortuneService2 implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return "Test error";
+	}
+	
+	
+
+}
