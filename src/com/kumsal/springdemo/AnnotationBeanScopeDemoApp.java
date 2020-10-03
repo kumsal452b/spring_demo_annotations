795909883986858f43bbcde9e3b1d013687bb261
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-03 12:32:02 +0300

    nothing

--- src/com/kumsal/springdemo/AnnotationBeanScopeDemoApp.java
+++ src/com/kumsal/springdemo/AnnotationBeanScopeDemoApp.java
@@ -5,8 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class AnnotationBeanScopeDemoApp {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		
 		Coach theCoach=context.getBean("tennisCoach",Coach.class);
 	
+		Coach alphaCoach=context.getBean("tennisCoach",Coach.class);
 	}
 
 }
