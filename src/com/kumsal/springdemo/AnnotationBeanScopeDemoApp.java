65faf99a88d43bb7263c4fb33836ede8778a30d1
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-03 12:31:04 +0300

    add new line

--- src/com/kumsal/springdemo/AnnotationBeanScopeDemoApp.java
+++ src/com/kumsal/springdemo/AnnotationBeanScopeDemoApp.java
@@ -1,5 +1,12 @@
 package com.kumsal.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AnnotationBeanScopeDemoApp {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		Coach theCoach=context.getBean("tennisCoach",Coach.class);
+	
+	}
 
 }
