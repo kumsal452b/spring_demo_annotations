4c3bf32ed7d888d18868965f5bb0b04cd23af8f2
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-03 12:59:15 +0300

    end ofday

--- src/com/kumsal/springdemo/TennisCoach.java
+++ src/com/kumsal/springdemo/TennisCoach.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
