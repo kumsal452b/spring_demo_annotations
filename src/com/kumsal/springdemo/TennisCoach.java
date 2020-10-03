4534c52679ee52ee146866ba0f2fdabaf53bef0f
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-03 12:30:21 +0300

    Nothing

--- src/com/kumsal/springdemo/TennisCoach.java
+++ src/com/kumsal/springdemo/TennisCoach.java
@@ -1,12 +1,14 @@
 package com.kumsal.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
 	@Autowired
+	@Qualifier("happyFortuneService2")
 	private FortuneService fortuneService;
 	
 //	public TennisCoach(FortuneService theFotuneservice) {
