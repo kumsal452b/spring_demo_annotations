6ff4f69c67e1e15cdd35eeeee31ae29b46e4f068
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-03 10:19:37 +0300

    changes continue

--- src/com/kumsal/springdemo/TennisCoach.java
+++ src/com/kumsal/springdemo/TennisCoach.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
-	@Autowired
+//	@Autowired
 	private FortuneService fortuneService;
 	
 //	public TennisCoach(FortuneService theFotuneservice) {
