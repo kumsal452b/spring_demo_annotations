a5079db70bbdec1c886afa329c3a806e32a542d7
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-04 21:17:34 +0300

    Update file

--- src/com/kumsal/springdemo/JavaConfigDemoApp.java
+++ src/com/kumsal/springdemo/JavaConfigDemoApp.java
@@ -1,10 +1,12 @@
 package com.kumsal.springdemo;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 public class JavaConfigDemoApp {
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(SportConfig.class);
+
 		Coach theCoach=context.getBean("tennisCoach",Coach.class);
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
