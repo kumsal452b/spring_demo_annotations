819b25198dfcdeacc0a15ee3074f6a8dfa4a9b3b
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-04 21:10:54 +0300

    add new annotations

--- src/com/kumsal/springdemo/SportConfig.java
+++ src/com/kumsal/springdemo/SportConfig.java
@@ -1,5 +1,10 @@
 package com.kumsal.springdemo;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.kumsal.springdemo")
 public class SportConfig {
 
 }
