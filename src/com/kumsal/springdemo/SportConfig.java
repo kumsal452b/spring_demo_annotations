1d5ba7509e2b1b31936a3d62835e699a3191a7b5
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-05 21:31:16 +0300

    updated

--- src/com/kumsal/springdemo/SportConfig.java
+++ src/com/kumsal/springdemo/SportConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.kumsal.springdemo")
+//@ComponentScan("com.kumsal.springdemo")
 public class SportConfig {
 	
 	@Bean
