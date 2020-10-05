113ae129d400122c21c6c320565356f2cec695f0
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-05 21:30:37 +0300

    Add new bean

---
+++ src/com/kumsal/springdemo/SwimJavaConfigDemoApp.java
@@ -0,0 +1,15 @@
+package com.kumsal.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class SwimJavaConfigDemoApp {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(SportConfig.class);
+
+		Coach theCoach=context.getBean("tennisCoach",Coach.class);
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		context.close();
+	}
+}
