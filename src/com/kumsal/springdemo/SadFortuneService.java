f62afc31b8c24153a2f2a6d64a91cdda934dbf4a
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-05 21:27:30 +0300

    nothing

--- src/com/kumsal/springdemo/SadFortuneService.java
+++ src/com/kumsal/springdemo/SadFortuneService.java
@@ -1,5 +1,8 @@
 package com.kumsal.springdemo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SadFortuneService implements FortuneService {
 
 	@Override
--- src/com/kumsal/springdemo/SportConfig.java
+++ src/com/kumsal/springdemo/SportConfig.java
@@ -1,10 +1,21 @@
 package com.kumsal.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.kumsal.springdemo")
 public class SportConfig {
+	
+	@Bean
+	public FortuneService sadFortuneService(){
+		return new SadFortuneService();
+	}
+	
+	@Bean
+	public Coach swinCoach(){
+		return new SwimCoach(sadFortuneService());
+	}
 
 }
